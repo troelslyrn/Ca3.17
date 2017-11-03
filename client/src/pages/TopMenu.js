e94e736d617dc3a11387bafb3225d0024e383177
Runfire22 <cwn951@live.dk>
2017-11-03 09:53:08 +0100

    now it looks great

--- client/src/pages/TopMenu.js
+++ client/src/pages/TopMenu.js
@@ -30,20 +30,28 @@ class TopMenu extends Component {
             </div>
             <ul className="nav navbar-nav">
               <li><Link to="/about">About</Link></li>
+              {this.state.isUser && (
               <li><Link to="/user">Page for Users </Link></li>
+              )}
+              {this.state.isAdmin && (
               <li><Link to="/admin">Page for Admins</Link></li>
+              )}
             </ul>
             <ul className="nav navbar-nav navbar-right">
               <li className="navbar-text" style={{ color: "steelBlue" }}>{logInStatus}</li>
-              <li><Link to="/register">Register</Link></li>
               <li>
-                {this.state.loggedIn ?
-                  (
-                    <Link to="/logout"><span className="glyphicon glyphicon-log-in"></span> Logout</Link>
-                  ) :
-                  (
+              {this.state.loggedIn ? (
+                    <li></li>
+            ) : (
+            <Link to="/register">Sign up</Link>
+              )}
+              </li>
+              <li>
+                {this.state.loggedIn ? (
+                    <Link to="/logout"><span className="glyphicon glyphicon-log-out"></span> Logout</Link>
+                  ) : (
                     <Link to="/login">
-                      <span className="glyphicon glyphicon-log-out"></span> Login </Link>
+                      <span className="glyphicon glyphicon-log-in"></span> Login </Link>
                   )}
               </li>
               
